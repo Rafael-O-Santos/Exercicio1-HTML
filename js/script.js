4241191713cc26bc5f1bdd7bd33d09bd7c1dd4ec
Rafael-O-Santos <raff2005oliveira@gmail.com>
2022-11-16 16:07:17 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*function clickMe(){
+    console.log("Fui clicado!")
+}
+
+function overMe(){
+    console.log("Mouse aqui!")
+} 
+
+function onFocus(){
+    console.log("Focus0")
+}
+
+function Blur(){
+    console.log("TO CEEEGO")
+}
+
+function Troca(){
+    console.log("troca!")
+}
+
+
+
+
+document.querySelector('#ex').innerHTML = "<h1>Papa Pio</h1>"*/
+
+
+function clicar(){
+    document.querySelector('.quadreido').innerText = "Aracnídeos são um grupo de animais invertebrados representado pelas aranhas, escorpiões, opiliões, ácaros e carrapatos. São agrupados na classe Arachnida, pertencente ao filo Arthropoda, diferindo das outras classes de artrópodes (insetos, crustáceos e etc) por não apresentarem antenas e mandíbulas e sim quelíceras, sendo assim chamados quelicerados."
+}
+
+function passando(){
+    document.querySelector('.quadreido').innerText = "As aranhas possuem até 8 pares de olhos simples, enquanto os escorpiões têm até 5 pares nas laterais do exoesqueleto, essas estruturas podem perceber movimentos."
+}
+
+function saindo(){
+    document.querySelector('.quadreido').innerHTML = "<h1>Aracnídeos</h1>"
+}
