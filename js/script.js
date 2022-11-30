15ef5d9370ff8486e38dfe3e7eee221c835e0492
RafaelO <raff2005oliveira@gmail.com>
2022-11-30 17:14:06 -0300

    JS SAAS

--- js/script.js
+++ js/script.js
@@ -17,14 +17,10 @@ function Blur(){
 function Troca(){
     console.log("troca!")
 }
-
-
-
-
 document.querySelector('#ex').innerHTML = "<h1>Papa Pio</h1>"*/
 
 
-function clicar(){
+/*function clicar(){
     document.querySelector('.quadreido').innerText = "Aracnídeos são um grupo de animais invertebrados representado pelas aranhas, escorpiões, opiliões, ácaros e carrapatos. São agrupados na classe Arachnida, pertencente ao filo Arthropoda, diferindo das outras classes de artrópodes (insetos, crustáceos e etc) por não apresentarem antenas e mandíbulas e sim quelíceras, sendo assim chamados quelicerados."
 }
 
@@ -32,6 +28,31 @@ function passando(){
     document.querySelector('.quadreido').innerText = "As aranhas possuem até 8 pares de olhos simples, enquanto os escorpiões têm até 5 pares nas laterais do exoesqueleto, essas estruturas podem perceber movimentos."
 }
 
+
 function saindo(){
     document.querySelector('.quadreido').innerHTML = "<h1>Aracnídeos</h1>"
+}*/
+
+
+
+function conta(){
+
+    let n1 = document.getElementById("n1").value
+    let n2 = document.getElementById("n2").value
+    let resultado = document.getElementById("resultado").innerHTML = parseInt(n1) + parseInt(n2)
+}
+
+
+let paragrafo = document.createElement("p")
+
+paragrafo.innerText = "Caneta Azul (Blue Pen) last sigma"
+document.body.appendChild(paragrafo)
+
+
+function minhaFuncao(){
+    document.getElementById("minhaDiv").classList.add("classe1","classe2","classe3")
+}
+
+function minhaFacao(){
+    document.getElementById("minhaDiv").classList.remove("classe1","classe2","classe3")
 }
---
+++ nova atividade JS/script.js
@@ -0,0 +1,15 @@
+function fundoAzul(){
+    document.getElementById('minhaDiv').classList.add("classe1")
+}
+
+function fundoTrans(){
+    document.getElementById('minhaDiv').classList.remove("classe1")
+}
+
+function fonteVerm(){
+    document.getElementById('minhaDiv').classList.add("classe2")
+}
+
+function fonteNorm(){
+    document.getElementById('minhaDiv').classList.remove("classe2")
+}
